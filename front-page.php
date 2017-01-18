e9b2a9b434e8fcf697770478c4ed30c4d51c9488
joshua906 <joshuamatthews906@yahoo.com>
2017-01-17 20:14:49 -0600

    fixed minor link issues

--- front-page.php
+++ front-page.php
@@ -16,75 +16,26 @@
  */
 get_header(); ?>
 
-<!-- temporary header
-<div class="hero-image">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12 text-center">
-        <div class="content">
-        <h1 id="demo" class="white-heading"></h1>
-        <a href="<?php echo get_page_link(8); ?>"><button class="button-more text-center">videos</button></a>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
--->
-<!--
 
-<div class="homepage-hero-module">
-    <div class="video-container">
-        <div class="filter"></div>
-        <div style="overflow: hidden; width: 100%; height: 600px;">
-        <video autoplay loop muted class="fillWidth">
-            <source src="<?php echo get_template_directory_uri(); ?>/video/bz-trailer-three.mp4" type="video/mp4" />
-            <source src="" type="video/webm" />
-        </video>
-        </div>
-        <div class="poster hidden">
-            <img src="<?php echo get_template_directory_uri(); ?>/brainzapped-theme/resources/img/new-img.jpg" alt="">
-        </div>
-    </div>
-</div>
--->
-<!--<div class="header-video">
-    <img src="https://static.pexels.com/photos/199470/pexels-photo-199470.jpeg"
-         class="header-video__media"
-         data-video-URL="<?php echo get_template_directory_uri(); ?>/video/bz-trailer-three.mp4"
-         data-teaser="video/teaser-video"
-         data-video-width="560"
-         data-video-height="315">
-    <a href="<?php echo get_template_directory_uri(); ?>/video/bz-trailer-three.mp4" class="header-video__play-trigger" id="header-video__play-trigger">Play video</a>
-    <button type="button" class="header-video__close-trigger" id="header-video__close-trigger">Close video</button>
-</div>-->
-
-<!-- hero video -->
 
 
 <div class="header-container">
 	<div class="video-container">
    	  <video class="video" preload="true" autoplay loop muted>
         	<source src="<?php echo get_template_directory_uri(); ?>/video/bz-trailer-three.mp4" type="video/mp4">
-           <!-- <source src="<?php echo get_template_directory_uri(); ?>/video/.webm" type="video/webm">
-          <source src="<?php echo get_template_directory_uri(); ?>/video/.ogv" type="video/ogg">-->
-          
       </video>
          <a href="<?php echo get_page_link(124); ?>"><button class="reel-button">Enter Site</button></a>
 
     </div>
 </div>
-<!--mobile Hero
-<div class="mobile-hero">
-  <div class="container">
-     <a href="<?php echo get_page_link(124); ?>"><button class="reel-button">Enter Site</button></a>
-  </div>
-</div>-->
+
 <div class="home-screen-footer">
   <div class="container">
     <div class="row">
       <div class="col-md-12">
-        <p style=" color: purple; font-size: 11px; padding: 12px 0px 10px 0px; text-align: center; text-transform: uppercase;">&copy; Brain Zapped 2017 // brainzappedtv@gmail.com // 214-284-3456 // Made by matthews designs  </p> 
+        <p style=" color: purple; font-size: 11px; padding: 12px 0px 10px 0px; text-align: center; text-transform: uppercase;">&copy; Brain Zapped 2017 //  brainzappedtv@gmail.com  //  214-284-3456  //  Made by matthews designs  </p> 
       </div>
     </div>
   </div>
 </div>
+<?php get_footer(); ?>
